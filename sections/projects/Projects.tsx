44c4756d6a38bbe9930f8731cde628eb07f465ca
SkyCaptainess <andychris9474@gmail.com>
2023-02-21 12:24:18 +0800

    Adjust margin bottom for project sub-heading

--- sections/projects/Projects.tsx
+++ sections/projects/Projects.tsx
@@ -34,13 +34,16 @@ const Projects = () => {
     <section>
       <div className="mx-auto mb-20 w-11/12 max-w-7xl sm:mb-28 2xl:mb-44">
         <Animation animation={animation} className="mb-8" viewAmount={0.7}>
-          <h2 ref={projectsRef} className="white-bg-heading">
+          <h2
+            ref={projectsRef}
+            className="white-bg-heading mb-5 text-lg md:mb-0 md:text-3xl xl:text-4xl"
+          >
             👇
             <span className="span-heading from-black-400 to-black-400">
               Projects
             </span>
           </h2>
-          <h3 className="white-sub-heading font-bold">
+          <h3 className="white-sub-heading text-lg font-bold md:text-3xl xl:text-4xl">
             Coding projects that I enjoyed working on.
           </h3>
         </Animation>
