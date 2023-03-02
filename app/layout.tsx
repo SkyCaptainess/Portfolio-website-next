1bd34c3f9f07c4ee7ca91e43071d03c6501a171c
SkyCaptainess <andychris9474@gmail.com>
2023-03-02 16:33:35 +0800

    Migrate metadata to layout with Metadata API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import { Montserrat } from '@next/font/google';
+import { Montserrat } from 'next/font/google';
+import { Metadata } from 'next';
 import { childrenNode } from '@/constants/typeInterface';
 import './globals.css';
 
@@ -8,6 +9,46 @@ const monserrat = Montserrat({
   style: ['normal', 'italic'],
   variable: '--font-mont',
 });
+
+export const metadata: Metadata = {
+  title: 'Nicholas Yong',
+  description:
+    'Hi, I\'m Nicholas and I love to build simple and beautiful things!',
+  creator: 'Nicholas Yong',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#f8f8f8' },
+    { media: '(prefers-color-scheme: dark)', color: '#212529' },
+  ],
+  icons: {
+    icon: '/favicons/favicon.ico',
+    apple: '/favicons/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicons/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicons/favicon-16x16.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '192x192',
+        url: '/favicons/android-chrome-192x192.png',
+      },
+      {
+        rel: 'mask-icon',
+        url: '/favicons/safari-pinned-tab.svg',
+      },
+    ],
+  },
+  manifest: '/favicons/site.webmanifest',
+};
 
 export default function RootLayout({ children }: childrenNode) {
   return (
